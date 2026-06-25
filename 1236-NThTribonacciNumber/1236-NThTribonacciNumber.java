864639c9ca01b827fddffc25ea1f8bc2a26b704a
dwaipayan-dev <51186857+dwaipayan-dev@users.noreply.github.com>
2026-06-25 08:36:21 +0530

    Create 1236-NThTribonacciNumber

---
+++ 1236-NThTribonacciNumber/1236-NThTribonacciNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 6/25/2026, 8:36:20 AM
+class Solution {
+    public int tribonacci(int n) {
+        int temp[] = new int[n+1];
+        if(n < 0) return -1;
+        temp[0] = 0;
+        if(n > 0)
+            temp[1] = 1;
+        if(n > 1)
+            temp[2] = 1;
+        for(int i = 3; i <= n; i++){
+            temp[i] = temp[i-3] + temp[i-2] + temp[i-1];
+        }
+        return temp[n];
+    }
+}
